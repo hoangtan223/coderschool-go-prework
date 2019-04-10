afcc8f05eeae4081ffea2e5d71670dab13d18c65
Travis223 <hoangtan223@gmail.com>
2019-04-10 09:51:14 +0700

    Finish prework

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+type responseInfo struct {
+	status   int
+	bytes    int64
+	duration time.Duration
+}
+
+type summaryInfo struct {
+	requested int64
+	responded int64
+}
+
+func main() {
+	fmt.Println("hello world")
+	requests := flag.Int64("n", 1, "Number of request to perform")
+	concurrency := flag.Int64("c", 1, "Number of multiple requests to make at a time")
+
+	fmt.Println(requests, concurrency)
+	flag.Parse()
+
+	if flag.NArg() == 0 || *requests == 0 || *requests < *concurrency {
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
+
+	link := flag.Arg(0)
+	c := make(chan responseInfo)
+	summary := summaryInfo{}
+	for i := int64(0); i < *concurrency; i++ {
+		summary.requested++
+		go checkLink(link, c)
+	}
+
+	for response := range c {
+		if summary.requested < *requests {
+			summary.requested++
+			go checkLink(link, c)
+		}
+
+		summary.responded++
+		fmt.Println(response)
+		if summary.requested == summary.responded {
+			break
+		}
+	}
+}
+
+func checkLink(link string, c chan responseInfo) {
+	start := time.Now()
+	res, err := http.Get(link)
+
+	if err != nil {
+		panic(err)
+	}
+
+	read, _ := io.Copy(ioutil.Discard, res.Body)
+	c <- responseInfo{
+		status:   res.StatusCode,
+		bytes:    read,
+		duration: time.Now().Sub(start),
+	}
+}
